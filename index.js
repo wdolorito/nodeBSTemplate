d1f1d257b9418cb19a446336a66849279be3a891
William Dolorito <wdolorito@gmail.com>
2018-09-01 14:04:23 -0700

    Removed extras

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 const server = require('http').createServer(app)
 const router = require('./routes')
 const fs = require('fs')
 let port = 3000
+app.use(express.static(__dirname + '/static'))
 
 try {
   port = fs.readFileSync('port', 'utf8')
@@ -13,9 +13,6 @@ try {
   console.log(error.stack)
 }
 
-app.use(express.static(__dirname + '/static'))
-app.use('/jspdf', express.static(__dirname + '/node_modules/jspdf/dist'))
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', router)
 
 server.listen(port, function() {
